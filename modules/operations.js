7cca5595cb41fd17a6413793f5a4841054389461
Maja Gedda <maja.gedda@zocom.se>
2020-11-12 09:17:16 +0100

    new file and math functions

---
+++ modules/operations.js
@@ -0,0 +1,6 @@
+let add = (num1, num2) => { return num1 + num2 };
+let sub = (num1, num2) => { return num1 - num2 };
+let div = (num1, num2) => { return num1 / num2 };
+let mul = (num1, num2) => { return num1 * num2 };
+
+export default {add, sub, div, mul};

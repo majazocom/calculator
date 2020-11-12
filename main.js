ca84ec53107cbce6950e4614853a768fb2eb9e22
Maja Gedda <maja.gedda@zocom.se>
2020-11-12 09:26:53 +0100

    changes

---
+++ main.js
@@ -0,0 +1,6 @@
+import { add, sub, div, mul } from './modules/operations.js';
+
+console.log(add(1, 2));
+console.log(sub(6, 2));
+console.log(div(4, 2));
+console.log(mul(10, 2));
--- modules/operations.js
+++ modules/operations.js
@@ -1,6 +1,4 @@
-let add = (num1, num2) => { return num1 + num2 };
-let sub = (num1, num2) => { return num1 - num2 };
-let div = (num1, num2) => { return num1 / num2 };
-let mul = (num1, num2) => { return num1 * num2 };
-
-export default {add, sub, div, mul};
+export function add(num1, num2) { return num1 + num2 };
+export function sub(num1, num2) { return num1 - num2 };
+export function div(num1, num2) { return num1 / num2 };
+export function mul(num1, num2) { return num1 * num2 };
